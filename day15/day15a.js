9b6bcf99b55eeee52625759ad68bdcab31b7e959
arcons <arcon1993@gmail.com>
2020-12-16 21:58:59 -0500

    15a 16a

---
+++ day15/day15a.js
@@ -0,0 +1,46 @@
+
+let input = [0,3,6]
+// let input = [2,1,3]
+// let input = [1,2,3]
+// let input = [2,3,1]
+// let input = [3,2,1]
+// let input = [3,1,2]
+// let input = [16,12,1,0,15,7,11]
+
+const checkRule = (lastSpoken, turn) => {
+  const lastSpokenIndex = input.lastIndexOf(lastSpoken)
+  if(input[turn-2] === input[turn-3]) {
+    return 1
+  }
+  input.splice(lastSpokenIndex, 1);
+  const prevSpoken = input.lastIndexOf(lastSpoken)
+  input.splice(lastSpokenIndex, 0, lastSpoken);
+  return (lastSpokenIndex-prevSpoken)
+}
+
+const runUntil2020 = () => {
+  let turnCount = 9
+  input.push(0)
+  let lastSpoken = input[input.length-1]
+  while(turnCount <= 2019) {
+    lastSpoken = checkRule(lastSpoken, turnCount)
+    if(input.includes(lastSpoken)) {
+      input.push(lastSpoken)
+    } else {
+      input.push(lastSpoken)
+      input.push(0)
+      lastSpoken = 0
+      turnCount++
+    }
+    // if(input.includes(lastSpoken)) {
+      // const lastSpokenIndex = input.indexOf(lastSpoken)
+      // input.splice(lastSpokenIndex, 1);
+    // }
+
+    turnCount++
+  }
+  console.log(input[2019])
+  return input[2019]
+}
+
+console.log(runUntil2020())
---
+++ day16/day16b.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+ 
+var contents = fs.readFileSync('./day16/testinput2.txt', 'utf8');
+// example parse
+const input = contents.split(/\n\s*\n/).map((str) => str.split(/\n+/))
+
+const parseRuleInput = (rule) => {
+  let firstRange = rule[1].split('-').map(val => parseInt(val))
+  let secondRange = rule[3].split('-').map(val => parseInt(val))
+  return [firstRange, secondRange]
+}
+const rules = input[0].map(val => val.split(' ')).map(rule => parseRuleInput(rule))
+const yourTicket = input[1][1].split(',').map(val=>parseInt(val))
+let nearbyTicket = input[2].slice(1).join(',').split(',').map(val => parseInt(val))
+
+
+const isBetweenRanges = (number) => {
+  let valid = false
+  valid = rules.some(rule => {
+    // console.log(rule[0][0])
+    if((rule[0][0] <= number && rule[0][1] >= number) || (rule[1][0] <= number && rule[1][1] >= number)){
+      return true
+    }
+  })
+  return valid
+}
+
+const findInvalidTickets = () => {
+  let invalidNumbers = []
+  nearbyTicket.forEach(ticket => {
+    if(!isBetweenRanges(ticket)){
+      invalidNumbers.push(ticket)
+    }
+  })
+  nearbyTicket.filter(ticket => invalidNumbers.includes(ticket))
+  console.log(nearbyTicket)
+}
+
+console.log(findInvalidTickets())
