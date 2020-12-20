de739ba262c59ce1b605f48ac88ee146e1695436
arcons <arcon1993@gmail.com>
2020-12-19 21:00:10 -0500

    Fixed day 16b

--- day16/day16b.js
+++ day16/day16b.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
  
-var contents = fs.readFileSync('./day16/testinput2.txt', 'utf8');
+var contents = fs.readFileSync('./day16/input.txt', 'utf8');
 // example parse
 const input = contents.split(/\n\s*\n/).map((str) => str.split(/\n+/))
 
@@ -45,35 +45,81 @@ const isValidForRules = (number) => {
   return validSet;
 }
 
-const getDifferenceBetweenColumns = (row, column, ruleSetArray) => {
-
+const getDifferenceBetweenColumns = (allRules) => {
+  // this isn't going to work due to the fact that there are some with 0, need to figure out how to ignore those
+  intersection = [...allRules].reduce((a, b) => {
+    // console.log(a, b)
+    if(b.size === 0) {
+      return a
+    }
+    return new Set([...a].filter(x => b.has(x)))
+  }
+  );
+  return [...intersection]
 }
 
-const rowLength = 3
-let realRules = new Array(rowLength)
-const findInvalidTickets = () => {
-  let ruleSetMap = []
+const numRows = nearbyTicket.length
+let realRules = new Array(yourTicket.length)
+
+const findDepartureTickets = () => {
+  let ruleSetColumns = new Array(yourTicket.length)
+  for(let i = 0; i < ruleSetColumns.length; i++) { 
+    ruleSetColumns[i]=[]
+  }
   nearbyTicket.forEach((row, rowIndex) => {
-    ruleSetMap.push(new Array(rowLength))
+    row.forEach((ticket,index) => {
+      if(!isBetweenRanges(ticket)){
+        row.pop(index)
+      }
+    })
     row.forEach((ticket, index)=> {
       // all validRules for each ticket
       let validRuleSet = isValidForRules(ticket);
       if(validRuleSet.size === 1) {
         realRules[index]=[...validRuleSet][0]
       }
-      ruleSetMap[rowIndex][index] = validRuleSet
+      ruleSetColumns[index].push(validRuleSet)
     })
   })
-  console.log(realRules)
-  // const filteredTicket = nearbyTicket.map(ticketRow => ticketRow.filter(ticket => !invalidNumbers.includes(ticket)))
-  console.log(ruleSetMap)
-  ruleSetMap.forEach((ruleSet,index) => {
-    [...ruleSet].forEach((set, column) => {
-      getDifferenceBetweenColumns(index, column, ruleSetMap)
-    })
+  ruleSetColumns.forEach((column,index) => {
+      let differences = getDifferenceBetweenColumns(column);
+      realRules[index] = differences
   })
 
-  return 0
+  let finalRules = new Array(yourTicket.length)
+  for(let i = 0; i < 20; i++ ) {
+    finalRules[i]=undefined
+  }
+  while(finalRules.includes(undefined)) {
+    realRules.forEach((rule, index) => {
+      rule = rule.filter(item => !finalRules.includes(item))
+      if(rule.length = 1) {
+        finalRules[index] = rule[0];
+      }
+    })
+  }
+
+  let total = 1
+  let totalDeparture = 1
+  for(let i = 0; i < 20; i++ ) {
+    if(finalRules[i] && finalRules[i].includes('departure')) {
+      // totalDeparture++
+      // console.log(totalDeparture)
+      console.log(yourTicket[i])
+      total *= yourTicket[i]
+    }
+  }
+  // 990612944677 too low
+  return total
 }
 
-console.log(findInvalidTickets())
+// function findYourTicketInvalid() {
+//   yourTicket.forEach((ticket,index) => {
+//     if(!isBetweenRanges(ticket)){
+//       yourTicket.pop(index)
+//     }
+//   })
+//   console.log(yourTicket)
+// }
+// console.log(findYourTicketInvalid())
+console.log(findDepartureTickets())
