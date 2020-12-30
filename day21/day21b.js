ac3dd88cfc095fe670476a99d9552efbf3d94189
arcons <arcon1993@gmail.com>
2020-12-30 10:15:10 -0500

    Fixed due to stupid bug

--- day21/day21b.js
+++ day21/day21b.js
@@ -28,74 +28,37 @@ const buildFoodList = () => {
   })
   // get a set of all the values
   // need to reduce the map and find which allergen matches to a ingredient
-  let output = []
   let foodOrgnaized = []
   foodAllergenMap.forEach((val, key, map) => {
     foodOrgnaized.push(key)
   })
-  let FOSet = [...foodOrgnaized.sort()]
+  //
   let all = []
-  FOSet.forEach(val => {
-    // all = new Set([...all, ...foodAllergenMap.get(val)])
-    all.push([...foodAllergenMap.get(val)])
-  })
-  all = all.flat()
-  // console.log(all.join(','))
-
-  // let testSet = new Set([...all])
-
-
-  all.forEach(val => output.includes(val) ? null : output.push(val))
-  // console.log([...new Set([...output])].join(','))
-
-  // foodAllergenMap.forEach((val, key, map) => {
-  //   map.forEach((food, allergen, currMap) => {
-  //     if(key != allergen) {
-  //       let difference = new Set(
-  //         [...food].filter(x => !val.has(x)));
-  //       console.log(difference)
-  //       map.set(allergen, difference)
-  //     }
-  //   })
-  // })
 
   let foodContains = []
-  let sortedMap = new Map([...foodAllergenMap.entries()].sort());
-  while(foodContains.length <= sortedMap.size) {
-    sortedMap.forEach((val, key, map) => {
+  while(foodContains.length <= foodAllergenMap.size) {
+    foodAllergenMap.forEach((val, key, map) => {
       if(val.size === 1) {
         const allergy = [...val][0]
         const keyAll = {key, allergy}
         map.forEach((food, allergen, currMap) => {
           if(food.has(allergy) && food.size !== 1) {
-            // let difference = new Set(
-            //   [...food].filter(x => !val.has(x)));
             food.delete(allergy)
-            // map.set(allergen, difference)
           }
         })
         if(!foodContains.includes(keyAll)){
           foodContains.push({key, allergy})
-          // foodContains.push({key, allergy})
         }
       }
     })
   }
   foodContains = []
-  foodAllergenMap.forEach((val, key, map) => {
+  let sortedMap = new Map([...foodAllergenMap.entries()].sort());
+  sortedMap.forEach((val, key, map) => {
     foodContains.push([...val])
   })
   all = foodContains.flat()
-  // console.log(all)
-  // FOSet = new Set([...all])
-  // FOSet.forEach(val => {
-  //   all.push([...foodAllergenMap.get(val)])
-  // })
-  // all = all.flat()
-  // wrong answers 
-  // mbdksj,rkzqs,zmhnj,zjsh,ctmzsr,hphcb,vrzkz,vzzxl
-  // zjsh,zmhnj,rkzqs,vrzkz,ctmzsr,vzzxl,hphcb,mbdksj
-  // vrzkz,zjsh,hphcb,mbdksj,vzzxl,ctmzsr,rkzqs,zmhnj
+
   return [...all].join(',')
 }
 
@@ -110,4 +73,3 @@ function compare( a, b ) {
 }
 
 console.log(buildFoodList())
-// console.log(foodList)
